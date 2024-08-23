ffe6f6cb5402109291629b149e006035ca593a97
phuc <phucpd@icheck.vn>
2024-08-24 01:37:07 +0700

    init2

--- src/utils/urlValidator.ts
+++ src/utils/urlValidator.ts
@@ -1,14 +1,17 @@
+// src/utils/urlValidator.ts
 export class UrlValidator {
-    private static urlPattern = new RegExp(
-      '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', 'i' // fragment locator
-    );
-  
-    public static isValidUrl(url: string): boolean {
-      return this.urlPattern.test(url);
-    }
+  private static urlPattern = new RegExp(
+    '^https?:\\/\\/' + // protocol
+    '((([a-zA-Z\\d]([a-zA-Z\\d-]*[a-zA-Z\\d])*)\\.)+[a-zA-Z]{2,}' + // domain name and extension
+    '|localhost|' + // or localhost
+    '\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|' + // or IPv4
+    '\\[?[a-fA-F\\d:]+\\]?)' + // or IPv6
+    '(\\:\\d+)?(\\/[-a-zA-Z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-zA-Z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-zA-Z\\d_]*)?$', 'i' // fragment locator
+  );
+
+  public static isValidUrl(url: string): boolean {
+    return this.urlPattern.test(url);
+  }
 }
